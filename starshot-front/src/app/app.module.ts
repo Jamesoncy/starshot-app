69f67639513438d0e00f46e9e8989d8ae046c90a
James Alden <JamesRoncy13@gmail.com>
2019-04-22 08:12:47 +0800

    develop: added components, dashboard login

--- starshot-front/src/app/app.module.ts
+++ starshot-front/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
